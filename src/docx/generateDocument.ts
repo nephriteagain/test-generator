a7d4f790d663910ea43f53500fd7ea3a4a445783
nephriteagain <oneabove232@gmail.com>
2023-08-15 15:57:15 +0800

    added styles

--- src/docx/generateDocument.ts
+++ src/docx/generateDocument.ts
@@ -1,80 +1,142 @@
-import type { test } from '@/app/types'
-
+import type { test } from '@/app/types';
 import {
     Document,
     Paragraph,
     Tab,
     TextRun,
-    Packer
-} from 'docx'
-import { saveAs } from 'file-saver'
+    Packer,
+    HeadingLevel,
+    LevelFormat,
+    AlignmentType,
+    BorderStyle
+} from 'docx';
+import { saveAs } from 'file-saver';
 
-
-export class TestAsDocX {    
+export class TestAsDocX {
     public document: Document;
 
-    constructor (test: test) {
-        const { units, subject, author } = test
+    constructor(test: test) {
+        const { units, subject, author } = test;
 
-        this.document = new Document({
+        this.document = new Document({            
+            numbering: {
+                config: [
+                    {
+                        reference: 'my-numbering',
+                        levels: [
+                            {
+                                level: 2,
+                                format: LevelFormat.LOWER_LETTER,                                
+                                alignment: AlignmentType.START,
+                                style: {
+                                    paragraph: {
+                                        indent: {
+                                             left: 1000,
+                                        }
+                                    }
+                                }
+                            },                            
+                        ]
+                    },
+                    {
+                        reference: 'my-numbering2',
+                        levels: [
+                            {
+                                level: 1,
+                                format: LevelFormat.DECIMAL,
+                                alignment: AlignmentType.START,
+                                style: {
+                                    paragraph: {
+                                        indent: {
+                                            left: 500,
+                                        }
+                                    }
+                                }                                
+                            }
+                        ]
+                    },                    
+                ]
+            },
             sections: [
                 {
                     children: [
-                        new Paragraph(subject),
-                        new Paragraph(author),
-                        ...units.map((unit) => {
-                            const { instructions, questions } = unit
-
-                            return new Paragraph({
-                                children: [
-                                    new TextRun({
-                                        text: instructions,
-                                        break: 1
-                                    }),
-
-                                    ...questions.map(q => {
-                                        const { question, choices } = q
-
-                                        return new Paragraph({
-                                            children: [
-                                                new TextRun({
-                                                    text: question,
-                                                    break: 1
-                                                }),
-                                                ...choices.map(c => {
-                                                    const { choice} = c
-
-                                                    return new Paragraph({
-                                                        children: [
-                                                            new TextRun({
-                                                                text: choice,
-                                                                break: 1
-                                                            })
-                                                        ]
-                                                    })
-                                                })
-                                            ]
-                                        })
-                                    })
-                                ]
-                            })
+                        new Paragraph({
+                            text: subject.trim(),
+                            heading: HeadingLevel.HEADING_1,                            
+                        }),
+                        new Paragraph({
+                            text: author.trim(),
+                            heading: HeadingLevel.HEADING_2,                                                        
+                            spacing: {
+                                after: 1000
+                            }   
+                        }),
+                        new Paragraph({
+                            children : [
+                                ...units.map((unit) => {
+                                    const { instructions, questions } = unit;
+        
+                                    return new Paragraph({
+                                        children: [
+                                            new Paragraph({
+                                                spacing: {
+                                                    after: 200
+                                                },
+                                                text: instructions
+                                            }),                                    
+                                            ...questions.map(q => {
+                                                const { question, choices } = q;
+        
+                                                return new Paragraph({
+                                                    children: [
+                                                        new Paragraph({
+                                                            spacing: {
+                                                                after:100
+                                                            },
+                                                            text: question.trim(),
+                                                            numbering: {
+                                                                reference: 'my-numbering2',
+                                                                level: 1
+                                                            }
+                                                        }),
+                                                        ...choices.map(c => {
+                                                            const { choice } = c;
+        
+                                                            return new TextRun({
+                                                                children: [
+                                                                    new Paragraph({
+                                                                        text: choice.trim(),
+                                                                        numbering: {
+                                                                            reference: 'my-numbering',
+                                                                            level: 2
+                                                                        }
+                                                                    }),
+                                                                ],
+                                                            });
+                                                        }),
+                                                    ],
+                                                });
+                                            }),
+                                        ],
+                                    });
+                                }),
+                            ]
                         })
-                    ]
-
-                    
-                }
-            ]
-        })
+                        
+                    ],
+                },
+            ],
+        });
     }
 
     download() {
         Packer.toBlob(this.document)
             .then(blob => {
-                saveAs(blob, 'test.docx')
-                console.log('Document created successfully')
+                saveAs(blob, 'test.docx');
+                console.log('Document created successfully');
             })
             .catch(err => {
-                console.log(err)
-            })
+                console.log(err);
+            });
     }
 }
